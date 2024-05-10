00d60994c6de9a1cf621dd5d66a723501cc748ab
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-05-10 20:17:00 +0300

    docs: add JSDoc comments to textSlicer function

--- src/utils/TextSlicer.ts
+++ src/utils/TextSlicer.ts
@@ -1,3 +1,10 @@
+/**
+ *
+ * @param {string} text - The text to be sliced.
+ * @param {number} [maxLength = 50] - The maximum length before truncation.
+ * @returns The sliced text, with an ellipsis (...) appended if truncated.
+ */
+
 export function textSlicer(text: string, maxLength: number = 50): string {
   if (text.length >= maxLength) return `${text.slice(0, maxLength)}...`
   return text
