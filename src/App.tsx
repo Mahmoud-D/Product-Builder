9b0605876559b3260028c8a8d6e914c1bda59074
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-05-13 23:55:16 +0300

    refactor: update modal functionality in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,25 @@ function App() {
       <div className="grid gap-3 p-2 m-5 rounded sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 ">
         {renderProductList}
       </div>
+      <Modal closeModal={closeModal} isOpen={isOpen}>
+        {renderFormInputList}
+        <div className="flex items-center justify-between space-x-2 ">
+          <Button
+            onClick={closeModal}
+            className="bg-indigo-600 hover:bg-indigo-800 "
+            width="w-full"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={closeModal}
+            className="bg-gray-600 hover:bg-gray-800 "
+            width="w-full"
+          >
+            Cancel
+          </Button>
+        </div>
+      </Modal>
     </main>
   )
 }
