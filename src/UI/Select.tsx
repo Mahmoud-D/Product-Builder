264027e49a681112bfb33616f59aa6d00f30ba16
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-05-17 05:53:49 +0300

    feat: Add Select component with Listbox functionality

---
+++ src/UI/Select.tsx
@@ -0,0 +1,102 @@
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import { categories } from '../data/index'
+import { TCategory } from '../types/TCategory'
+
+type TProps = {
+  selected: TCategory
+  setSelected: (category: TCategory) => void
+}
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+const Select = ({ selected, setSelected }: TProps) => {
+  //   const [selected, setSelected] = useState(categories[0])
+  return (
+    <Listbox value={selected} onChange={setSelected}>
+      {({ open }) => (
+        <>
+          <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
+            Category
+          </Listbox.Label>
+          <div className="relative mt-2">
+            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+              <span className="flex items-center">
+                <img
+                  src={selected.imageURL}
+                  alt=""
+                  className="flex-shrink-0 w-5 h-5 rounded-full"
+                />
+                <span className="block ml-3 truncate">{selected.name}</span>
+              </span>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-2 ml-3 pointer-events-none">
+                <ChevronUpDownIcon
+                  className="w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+
+            <Transition
+              show={open}
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-56 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {categories.map((category) => (
+                  <Listbox.Option
+                    key={category.id}
+                    className={({ active }) =>
+                      classNames(
+                        active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                        'relative cursor-default select-none py-2 pl-3 pr-9'
+                      )
+                    }
+                    value={category}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex items-center">
+                          <img
+                            src={category.imageURL}
+                            alt=""
+                            className="flex-shrink-0 w-5 h-5 rounded-full"
+                          />
+                          <span
+                            className={classNames(
+                              selected ? 'font-semibold' : 'font-normal',
+                              'ml-3 block truncate'
+                            )}
+                          >
+                            {category.name}
+                          </span>
+                        </div>
+
+                        {selected ? (
+                          <span
+                            className={classNames(
+                              active ? 'text-white' : 'text-indigo-600',
+                              'absolute inset-y-0 right-0 flex items-center pr-4'
+                            )}
+                          >
+                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </>
+      )}
+    </Listbox>
+  )
+}
+
+export default Select
